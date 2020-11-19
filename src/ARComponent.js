524d6c7c4eb8b4390fe633b4243846acd67e1f74
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-11-19 23:25:39 +0100

    push

---
+++ src/ARComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { OrbitControls, Box } from "drei";
+import { Canvas } from "react-three-fiber";
+import { ARCanvas } from "@react-three/xr";
+
+
+export default function AR() {
+  return (
+    <ARCanvas>
+      {/* Scene lighting */}
+      <ambientLight />
+      <spotLight />
+
+      {/* Controll camera with mouse */}
+      <OrbitControls />
+
+      {/* Some geometry */}
+      <Box>
+        <meshStandardMaterial color="#e23" />
+      </Box>
+    </ARCanvas>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import "./App.css";
-import "aframe";
-import {
-  // Box,
-  // Sphere,
-  // Cylinder,
-  // Plane,
-  // Sky,
-  // Text,
-  // Scene,
-  AFrameRenderer,
-  Marker
-} from "react-web-ar";
+
+import ARComponent from './ARComponent'
 
 function App() {
   return (
-    <div>
-    <AFrameRenderer>
-      <Marker parameters={{preset:"hiro"}}>
-        <a-box color="blue">
-
-        </a-box>
-      </Marker>
-    </AFrameRenderer>
+    <div className="App">
+      <ARComponent/>
     </div>
   );
 }
