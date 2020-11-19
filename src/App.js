37e2d9fcce4f698938e4ac92a136801b2a27f381
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-11-20 00:44:38 +0100

    fixed title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       {/* <ARComponent/> */}
-      <iframe id="f3b9d1ee-2edc-46f0-8c6f-5c7a75dca77b" src="https://www.vectary.com/viewer/v1/?model=f3b9d1ee-2edc-46f0-8c6f-5c7a75dca77b&env=studio3" frameborder="0" width="100%" height="480"></iframe>
+      <iframe title="picture" id="f3b9d1ee-2edc-46f0-8c6f-5c7a75dca77b" src="https://www.vectary.com/viewer/v1/?model=f3b9d1ee-2edc-46f0-8c6f-5c7a75dca77b&env=studio3" frameborder="0" width="100%" height="480"></iframe>
     </div>
   );
 }
