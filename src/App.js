0cc1f869fa6bc7bbb73c1180762293468f04ea14
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-11-20 01:07:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 
 // import ARComponent from './ARComponent'
+import Model from './assets/models/test.usdz'
 
 function App() {
   return (
     <div className="App">
       {/* <ARComponent/> */}
-      <iframe title="picture" id="f3b9d1ee-2edc-46f0-8c6f-5c7a75dca77b" src="https://www.vectary.com/viewer/v1/?model=f3b9d1ee-2edc-46f0-8c6f-5c7a75dca77b&env=studio3" frameborder="0" width="100%" height="480"></iframe>
+      <a href={Model} rel="al">test</a>
     </div>
   );
 }
