8e6cefce814731e7fa6c99837337ac4e231f89b3
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-11-20 02:09:35 +0100

    two pictures

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 // import ARComponent from './ARComponent'
 import Model from './assets/models/test.usdz'
+import Model2 from './assets/models/frame.usdz'
 
 function App() {
   return (
     <div className="App">
       {/* <ARComponent/> */}
-      <a href={Model} rel="al">test</a>
+      <a href={Model} rel="al">Wall Picture</a>
+      <a href={Model} rel="al">Table Picture</a>
     </div>
   );
 }
