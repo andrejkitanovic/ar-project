4d765129689334205e2a30da0e7a023276f93540
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-11-20 21:40:07 +0100

    usdz file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 // import Model2 from './assets/models/frame.usdz'
 // import usdz from "./test.usdz";
 import glb from './out.glb'
-import usdz from './out.usdz'
+import usdz from './testusdz.usdz'
 
 // import "@google/model-viewer";
 // import { ModelViewer, ARButton } from "@real2u/react-ar-components";
