0955e4df40f0bf475b393d096abdba4754d807d7
andrejkitanovic <kitanovicandrej213@gmail.com>
2020-11-21 08:53:34 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css'
+import "./App.css";
 
-// import ARComponent from './ARComponent'
-// import Model from './assets/models/test.usdz'
-// import Model2 from './assets/models/frame.usdz'
-// import usdz from "./test.usdz";
-import glb from './test.glb'
-import usdz from './proba.usdz'
+import glb from "./assets/models/test.glb";
+import usdz from "./assets/models/test.usdz";
 
-// import "@google/model-viewer";
 // import { ModelViewer, ARButton } from "@real2u/react-ar-components";
 
-import {  isIOS } from 'react-device-detect'
+import { isIOS, isAndroid } from "react-device-detect";
+
+import example from "./assets/images/example.png";
+import androidios from "./assets/images/androidios.png";
 
 // import Image from './image.jpg'
 // import { useState } from "react";
@@ -19,29 +17,47 @@ function App() {
   // const [displayPicture, setDisplayPicture] = useState(false);
   return (
     <div className="App">
-      <div className="image">
+      <div className="modal">
+        <div className="picture">
+          <h1>Girl with a heart</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <div className="circle">
+            <div className="circle-ar">
+              <span>AR</span>
+            </div>
+            <div className="container">
+              <img src={example} alt="Example usage" />
+              <div className="usage">
+                <p>Place your picture in real world with only one click!</p>
+                <p className="supported">Supported:</p>
+                <img src={androidios} alt="Android IOS" />
+              </div>
+            </div>
+             {isIOS && <a href={usdz} className="view">View model in 3d</a>}
+             {isAndroid && <model-viewer ar src={glb} alt="this is a 3d model"></model-viewer>}
+          </div>
+        </div>
+      </div>
+      {/* <div className="image">
         <div className="choosen-image">
-          <model-viewer ar src={glb} alt="this is a 3d model"></model-viewer>
-          {/* <img src={Image} alt="gallery"/> */}
+          
+     
         </div>
 
-        {/* {isBrowser && (
-          <button className="view" onClick={() => setDisplayPicture(true)}>
-            View model in 3d
-          </button>
-        )}
-        {isMobile && (
-          <button className="view">
-            <ARButton glb={glb} usdz={usdz}>
-            </ARButton>
-          </button>
-        )} */}
-        {isIOS && <a href={usdz} className="view">View model in 3d</a>}
-      </div>
-
-    
+   
+       
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
